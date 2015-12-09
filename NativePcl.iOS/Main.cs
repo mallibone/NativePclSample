c41424ebc8597f0efdf4c5e852e0d921554dd7a4
Mark <sirkenix@gmail.com>
2015-12-09 21:58:17 +0100

    Adds iOS Sample

---
+++ NativePcl.iOS/Main.cs
@@ -0,0 +1,19 @@
+﻿using UIKit;
+using NativePcl.Core.ViewModel;
+using System;
+
+namespace NativePcl.iOS
+{
+	public class Application
+	{
+		private static Lazy<ViewModelLocator> _locator = new Lazy<ViewModelLocator>(() => new ViewModelLocator());
+		public static ViewModelLocator Locator = _locator.Value;
+		// This is the main entry point of the application.
+		static void Main (string[] args)
+		{
+			// if you want to use a different Application Delegate class from "AppDelegate"
+			// you can specify it here.
+			UIApplication.Main (args, null, "AppDelegate");
+		}
+	}
+}
---
+++ NativePcl.iOS/ViewController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using NativePcl.Core.ViewModel;
+
+using UIKit;
+using GalaSoft.MvvmLight.Helpers;
+
+namespace NativePcl.iOS
+{
+	public partial class ViewController : UIViewController
+	{
+		Binding<string, string> _osVersionBinding;
+
+		public ViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		private MainViewModel Vm => Application.Locator.Main;
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			// Perform any additional setup after loading the view, typically from a nib.
+			_osVersionBinding = this.SetBinding(() => Vm.OSVersion, () => OSVersion.Text);
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			base.DidReceiveMemoryWarning ();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
+
